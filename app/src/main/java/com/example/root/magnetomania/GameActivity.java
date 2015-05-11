75e983e96132794d2654203f6a088996a0d96b7e
Karan Desai <karandesai_96@live.com>
2015-05-11 10:26:36 +0530

    Initial Commit.

---
+++ app/src/main/java/com/example/root/magnetomania/GameActivity.java
@@ -0,0 +1,15 @@
+package com.example.root.magnetomania;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+}
---
+++ app/src/main/java/com/example/root/magnetomania/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.root.magnetomania;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    Button playButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickPlay(View view)
+    {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+}
